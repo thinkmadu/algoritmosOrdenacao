1f7ce4cacf77a2de2d2cbd525dadb68918450fc8
Maria Eduarda <41195347+thinkmadu@users.noreply.github.com>
2023-10-21 18:05:31 -0300

    Update CountingSort.java
    Adicionei o código em java do CoutingSort

--- Códigos/Java/CoutingSort.java
+++ Códigos/Java/CoutingSort.java
@@ -1,2 +1,36 @@
 public class CoutingSort {
-}
+    public static int numMax(int[] vetor) {
+        if (vetor.length == 0) {
+            return 0;
+        }
+
+        int max = vetor[0];
+
+        for (int i = 1; i < vetor.length; i++) {
+            if (max < vetor[i]) {
+                max = vetor[i];
+            }
+        }
+        return max;
+    } //fim do método
+
+    public static int[] countingSort(int[] vetor) {
+        int aux = numMax(vetor);
+        int n = (vetor.length > aux) ? vetor.length : aux + 1;
+        int[] auxVetor = new int[n];
+        for (int k : vetor) {
+            auxVetor[k]++;
+        }
+
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (auxVetor[i] != 0) {
+                vetor[j] = i;
+                auxVetor[i]--;
+                j++;
+            }
+        }
+        return vetor;
+    } //fim do método
+} //fim da classe
